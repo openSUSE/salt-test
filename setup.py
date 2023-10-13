22a050fba763c7b6745c06c7f82b038c129d657e
Alexander Graul <agraul@suse.com>
2023-10-13 11:28:47 +0200

    Fix setup.py & rename to salt_test

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="salt-tests-launcher",
+    name="salt-test",
     version="1.0",
     install_requires=["toml"],
     package_dir={"": "src"},  # not needed for setuptools >= 61
-    packages=find_packages(),
+    packages=find_packages("src"),
     entry_points={
         "console_scripts": [
-            "salt-test = salt_tests_launcher:main",
+            "salt-test = salt_test:main",
         ]
     },
 )
