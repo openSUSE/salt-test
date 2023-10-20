bee1080d286f7bd4b935c06e1599cc38d4d152c6
Alexander Graul <agraul@suse.com>
2023-10-20 16:49:08 +0200

    Add salt_test module and tests

---
+++ src/salt_test/__init__.py
@@ -0,0 +1,220 @@
+"""Run Salt Test Suite by test group."""
+import os
+import pathlib
+import re
+import subprocess
+import sys
+import typing
+import urllib.request
+from argparse import ArgumentParser, FileType
+from collections import defaultdict
+
+try:
+    import tomllib  # available in Python 3.11+
+
+    toml = tomllib
+except ImportError:
+    import tomli
+
+    toml = tomli
+
+
+DEFAULT_CONFIG = {
+    "unit": {"dirs": ["tests/unit/", "tests/pytests/unit/"], "pytest_args": []},
+    "functional": {"dirs": ["tests/pytests/functional"], "pytest_args": []},
+    "integration": {
+        "dirs": ["tests/integration/", "tests/pytests/integration/"],
+        "pytest_args": [],
+    },
+    "scenarios": {"dirs": ["tests/pytests/scenarios/"], "pytest_args": ["--slow"]},
+}
+
+FLAVOR_RPM = {
+    "classic": "python3-salt-testsuite",
+    "bundle": "venv-salt-minion-testsuite",
+}
+
+
+def parse_skiplist(
+    file: typing.BinaryIO, groups: typing.Iterable[str]
+) -> typing.Dict[str, typing.List[str]]:
+    """Reads skiplist and returns a dictionary.
+
+    The dictionary fills in missing test groups to guarantee [] access.
+    """
+    raw_dict = toml.load(file)
+    # raw_dict: {"ignore": {$group: [...]}, "skip": { $group: [...]}}
+    ignored_files = raw_dict.get("ignore", {})
+    skipped_tests = raw_dict.get("skip", {})
+
+    # skiplist: {$group: {"ignore": [...], "skip": [...]}}
+    skiplist = {}
+    for group in groups:
+        skiplist[group] = {
+            "ignore": ignored_files.get(group, []),
+            "skip": skipped_tests.get(group, []),
+        }
+    return skiplist
+
+
+def parse_config(file: typing.BinaryIO):
+    """Reads the config and returns a dictionary.
+
+    The dictionary fills in missing test groups to guarantee [] access.
+    """
+    raw_dict = toml.load(file)
+    # raw_dict: {groups: {$group: {"dirs": [...], "pytest_args": [...]}
+
+    config = {}
+    for group in raw_dict["groups"]:
+        config[group]["dirs"] = group["dirs"]
+        config[group]["pytest_args"] = group.get("pytest_args", [])
+
+    return config
+
+
+def find_testsuite_root(flavor: str) -> str:
+    """Find root of test suite based.
+
+    :param flavor: One of "classic", "bundle".
+    """
+
+    def _list_files(rpm: str) -> typing.List[str]:
+        cp = subprocess.run(
+            ["rpm", "-q", "-l", rpm],
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+        )
+        if cp.returncode != 0:
+            return []
+        return cp.stdout.split()
+
+    pkg = FLAVOR_RPM[flavor]
+    pkg_files = _list_files(pkg)
+    root = None
+    for file in pkg_files:
+        match = re.match(r"(/usr/lib/.*/site-packages/salt-testsuite)", file)
+        if match:
+            root = match.group(0)
+            break
+
+    if root is None:
+        raise RuntimeError(f'Test suite for flavor "{flavor}" not installed.')
+
+    return root
+
+
+def testsuite_root_to_bindir(root: str) -> str:
+    """Compute /bin from the testsuite root.
+
+    This is needed to use the correct pytest executable.
+    """
+    p = pathlib.Path(root)
+    while p.name != "lib":
+        p = p.parent
+    # p now ends with /lib, therefore bin is a sibling
+    return str(p.parent / "bin")
+
+
+def pytest_cmd(group: str, skiplist: dict, config: dict, extra_args: typing.List[str]):
+    """Compose the correct command args for pytest."""
+    cmd = ["pytest"]
+    if not extra_args:
+        extra_args = config[group]["pytest_args"]
+    cmd.extend(extra_args)
+    for skipped_file in skiplist[group]["ignore"]:
+        cmd.extend(["--ignore", skipped_file])
+    for skipped_test in skiplist[group]["skip"]:
+        cmd.extend(["--deselect", skipped_test])
+
+    # test dirs should follow CLI flags
+    cmd.extend(config[group]["dirs"])
+
+    return cmd
+
+
+def prepare_argparser() -> ArgumentParser:
+    parser = ArgumentParser("salt-test")
+    parser.add_argument(
+        "--skiplist",
+        "-s",
+        type=str,
+        required=True,
+        help="Specify location of skiplist (TOML). Can be a HTTP URL.",
+    )
+    parser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        help="Specify location of config (TOML). Can be a HTTP URL.",
+    )
+    parser.add_argument(
+        "--package-flavor",
+        "-f",
+        choices=tuple(FLAVOR_RPM.keys()),
+        default="bundle",
+        help="Used to determine the Python environment that includes dependencies.",
+    )
+    parser.add_argument(
+        "--directory",
+        "-d",
+        type=str,
+        help="Root of test suite. Can be used to override the test suite root computed based on the --package-flavor.",
+    )
+    parser.add_argument("test_group", help="Group of tests to run.")
+    parser.add_argument(
+        "pytest_args",
+        nargs="*",
+        help="Specify extra arguments for pytest, separated from 'test_group' by a single --.",
+    )
+    return parser
+
+
+def update_env(env: os._Environ, bindir: str) -> dict:
+    env_copy = env.copy()
+    env_copy["PATH"] = f"{bindir}:{env_copy['PATH']}"
+    return env_copy
+
+
+def main():
+    parser = prepare_argparser()
+    args = parser.parse_args()
+
+    if args.config:
+        if args.config.startswith("http"):
+            with urllib.request.urlopen(args.config) as f:
+                config = parse_config(f)
+        else:
+            with open(args.config, "rb") as f:
+                config = parse_config(f)
+    else:
+        config = DEFAULT_CONFIG
+
+    if args.skiplist.startswith("http"):
+        with urllib.request.urlopen(args.skiplist) as f:
+            skiplist = parse_skiplist(f, config.keys())
+    else:
+        with open(args.skiplist, "rb") as f:
+            skiplist = parse_skiplist(f, config.keys())
+
+    try:
+        testsuite_root = find_testsuite_root(args.package_flavor)
+    except RuntimeError as e:
+        print(e)
+        sys.exit(1)
+
+    if args.directory:
+        cwd = args.directory
+    else:
+        cwd = testsuite_root
+
+    bindir = testsuite_root_to_bindir(testsuite_root)
+
+    env = update_env(os.environ, bindir)
+    cmd = pytest_cmd(args.test_group, skiplist, config, args.pytest_args)
+    print("Running:", " ".join(cmd))
+    subprocess.run(
+        cmd,
+        env=env,
+        cwd=cwd,
+    )
---
+++ tests/test_salt_test.py
@@ -0,0 +1,113 @@
+"""Unit tests for salt_test"""
+import argparse
+import io
+
+import pytest
+import salt_test
+
+
+def test_prepare_argparser():
+    parser = salt_test.prepare_argparser()
+    args = parser.parse_args(
+        [
+            "-s",
+            "skipped.toml",
+            "-c",
+            "config.toml",
+            "-f",
+            "classic",
+            "unit",
+            "--",
+            "--slow",
+        ]
+    )
+    assert args == argparse.Namespace(
+        skiplist="skipped.toml",
+        config="config.toml",
+        directory=None,
+        package_flavor="classic",
+        test_group="unit",
+        pytest_args=["--slow"],
+    )
+
+    args = parser.parse_args(
+        ["-s", "skipped.toml", "-c", "config.toml", "-d", "/code/", "functional"]
+    )
+    assert args == argparse.Namespace(
+        skiplist="skipped.toml",
+        directory="/code/",
+        config="config.toml",
+        package_flavor="bundle",
+        test_group="functional",
+        pytest_args=[],
+    )
+
+
+skiplist_empty = io.BytesIO(
+    b"""\
+"""
+)
+skiplist = io.BytesIO(
+    b"""\
+[ignore]
+unit = [
+	"tests/unit/modules/test_boto3_elasticsearch.py",
+	"tests/unit/modules/test_boto3_route53.py",
+	"tests/unit/modules/test_boto_route53.py",
+	"tests/unit/utils/test_boto3mod.py",
+]
+[skip]
+unit = [
+	"tests/unit/test_config.py::SampleConfTest::test_conf_master_sample_is_commented",
+	"tests/unit/test_config.py::ConfigTestCase::test_load_minion_config_from_environ_var",
+	"tests/unit/test_config.py::APIConfigTestCase::test_api_config_log_file_values",
+	"tests/unit/test_config.py::APIConfigTestCase::test_api_config_prepend_root_dirs_return",
+	"tests/unit/cli/test_support.py::ProfileIntegrityTestCase::test_jobs_trace_template_profile",
+]
+functional = [
+	"tests/pytests/functional/channel/test_server.py::test_pub_server_channel[transport(zeromq)]",
+	"tests/pytests/functional/modules/test_sdb.py::test_setting_sdb_values_with_text_and_bytes_should_retain_data_types[bang]",
+	"tests/pytests/functional/modules/test_sdb.py::test_setting_sdb_values_with_text_and_bytes_should_retain_data_types[expected_value2]",
+]
+"""
+)
+
+
+def test_parse_skiplist():
+    groups = ["unit", "integration", "functional", "scenarios"]
+    empty = salt_test.parse_skiplist(skiplist_empty, groups)
+    assert empty == {
+        "unit": {"ignore": [], "skip": []},
+        "integration": {"ignore": [], "skip": []},
+        "functional": {"ignore": [], "skip": []},
+        "scenarios": {"ignore": [], "skip": []},
+    }
+
+    populated = salt_test.parse_skiplist(skiplist, groups)
+    assert populated == {
+        "unit": {
+            "ignore": [
+                "tests/unit/modules/test_boto3_elasticsearch.py",
+                "tests/unit/modules/test_boto3_route53.py",
+                "tests/unit/modules/test_boto_route53.py",
+                "tests/unit/utils/test_boto3mod.py",
+            ],
+            "skip": [
+                "tests/unit/test_config.py::SampleConfTest::test_conf_master_sample_is_commented",
+                "tests/unit/test_config.py::ConfigTestCase::test_load_minion_config_from_environ_var",
+                "tests/unit/test_config.py::APIConfigTestCase::test_api_config_log_file_values",
+                "tests/unit/test_config.py::APIConfigTestCase::test_api_config_prepend_root_dirs_return",
+                "tests/unit/cli/test_support.py::ProfileIntegrityTestCase::test_jobs_trace_template_profile",
+            ],
+        },
+        "integration": {"ignore": [], "skip": []},
+        "functional": {
+            "ignore": [],
+            "skip": [
+                "tests/pytests/functional/channel/test_server.py::test_pub_server_channel[transport(zeromq)]",
+                "tests/pytests/functional/modules/test_sdb.py::test_setting_sdb_values_with_text_and_bytes_should_retain_data_types[bang]",
+                "tests/pytests/functional/modules/test_sdb.py::test_setting_sdb_values_with_text_and_bytes_should_retain_data_types[expected_value2]",
+            ],
+        },
+        "scenarios": {"ignore": [], "skip": []},
+    }
