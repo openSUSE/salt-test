fc872733031d1d274cd97c4002aeb2f0f58aecdf
Alexander Graul <agraul@suse.com>
2023-10-20 16:47:23 +0200

    Add packaging metadata

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="salt-test",
+    version="1.0",
+    install_requires=["tomli;python_version<'3.11'"],
+    package_dir={"": "src"},  # not needed for setuptools >= 61
+    packages=find_packages("src"),
+    entry_points={
+        "console_scripts": [
+            "salt-test = salt_test:main",
+        ]
+    },
+)
