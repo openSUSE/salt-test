3675ce0cf03cb797d8eb228463e9751cf360351e
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-10-30 12:53:22 +0000

    Bump version to 1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="salt-test",
-    version="1.6",
+    version="1.7",
     install_requires=["tomli;python_version<'3.11'", "requests"],
     package_dir={"": "src"},  # not needed for setuptools >= 61
     packages=find_packages("src"),
